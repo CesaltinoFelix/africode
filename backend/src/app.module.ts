5e37493a09e83c46282ecad80edb5b3423bd30c5
cesaltinofelix <cesaltinofelix2000@gmail.com>
2025-12-10 15:42:28 +0100

    feat: backend init (core)

---
+++ backend/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { EstablishmentModule } from './modules/establishment/establishment.module';
+import { QueueModule } from './modules/queue/queue.module';
+import { TicketModule } from './modules/ticket/ticket.module';
+
+@Module({
+  imports: [UserModule, EstablishmentModule, QueueModule, TicketModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
